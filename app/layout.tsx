f49efdfbf4ec2f2295d5dc89876f69d70ad82893
Temporary User <andrestran@icloud.com>
2025-11-17 19:58:19 -0600

    Adjust global layout top padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
           defaultTheme="system"
         >
           <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
-            <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-10">
+            <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-4">
               <Header />
               {children}
               <FooterWrapper />
