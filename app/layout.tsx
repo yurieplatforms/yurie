db8a07b9a048c06e66058e0dde38ffb0891b8386
Temporary User <andrestran@icloud.com>
2025-11-19 21:44:46 -0600

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 import { Header } from './header'
 import { FooterWrapper } from './footer-wrapper'
@@ -64,6 +65,7 @@ export default function RootLayout({
             </div>
           </div>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
