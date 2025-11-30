600c48de0cc6917a0b543b456b593321faf4cdd6
yurieplatforms <230779210+yurieplatforms@users.noreply.github.com>
2025-11-30 01:50:16 -0500

    fix(utils): Correctly spread inputs in cn function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,7 @@ import { ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(...inputs))
 }
 
 export const readFileAsDataURL = (file: File) =>
