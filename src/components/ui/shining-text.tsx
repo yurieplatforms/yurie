d896606434b5f356c77fe1ec8ed5c7c816fe3159
Temporary User <andrestran@icloud.com>
2025-12-03 23:06:32 -0600

    Fix import path in shining-text.tsx and remove stale lib files

--- src/components/ui/shining-text.tsx
+++ src/components/ui/shining-text.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { motion } from "motion/react"
-import { cn } from "@/lib/utils"
+import { cn } from "@/utils"
 
 interface ShiningTextProps {
   text: string
